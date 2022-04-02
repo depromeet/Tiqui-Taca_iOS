ad795fe7c89b0bb01a49b5777e49d0790350f9c6
KangMinseok <qbq5000@gmail.com>
2022-04-02 22:28:01 +0900

    🎉 프로젝트 파일 추가

---
+++ TiquiTaca_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
