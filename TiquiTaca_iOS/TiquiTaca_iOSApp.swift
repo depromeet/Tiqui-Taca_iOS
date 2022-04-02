2039ed941ca8218887b3ebed4fe0d231ab97990b
Minseok Kang <qbq5000@gmail.com>
2022-04-03 00:22:16 +0900

    0.1/merge master into release (#11)
    * 🎉 프로젝트 파일 추가 (#8)
    
    * 서브 모듈 추가 (#10)
    
    * ✨ 서브 모듈 추가
    
    + 디자인 시스템 모듈 추가
    + 네트워크 모듈 추가
    
    * 🔖 최소 배포 버전 변경
    
    * 🔖 버전 수정

---
+++ TiquiTaca_iOS/TiquiTaca_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiquiTaca_iOSApp.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/04/02.
+//
+
+import SwiftUI
+
+@main
+struct TiquiTaca_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
