0c1dd62d1a879694bbbb224f47b2bb43cba8e4e7
Minseok Kang <qbq5000@gmail.com>
2022-04-17 12:55:16 +0900

    Structure Configuration (#24)
    * 📦 Package 추가
    
    * 🎨 폴더 구조 변경 및 Info.plist 추가
    
    * 🙈 .gitignore 수정
    
    * 📦 R.swift 추가
    
    * 💚 Github Actions 가상머신 Xcode version 수정
    
    * 🔥 기존 SPM 패키지 제거
    
    * 🙈 Cocoapods 적용을 위한 gitignore 수정
    
    * ✨ SPM -> Cocoapods로 전환
    
    * 💚 CocoaPods 변경으로인한 빌드 수정
    
    * ⚡️ CocoaPods 빌드 속도 이슈 수정
    
    * ⚡️ CocoaPods Cache 추가
    
    * 💚 pod install 분리
    
    * ✨ LaunchScreen 및 AppDelegate adapter 추가
    
    * 💄 lint 조건 수정
    
    * 🎨 폴더유지를 위한 gitkeep 추가
    
    * 📦 NetworkModule 업데이트

---
+++ TiquiTaca_iOS/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/04/17.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    print("Called: didFinishLaunchingWithOptions")
+    return true
+  }
+}
