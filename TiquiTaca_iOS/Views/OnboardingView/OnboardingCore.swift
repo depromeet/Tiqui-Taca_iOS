5ca2f2b0bc43273fcd6a6525d1570f50d35e4276
RokwonK <wonrok97@naver.com>
2022-04-23 20:09:55 +0900

    🎨 OnboardingView TCA화

---
+++ TiquiTaca_iOS/Views/OnboardingView/OnboardingCore.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingCore.swift
+//  TiquiTaca_iOS
+//
+//  Created by 김록원 on 2022/04/23.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct OnboardingState: Equatable {
+	var currentPage: Int
+}
+
+enum OnboardingAction: Equatable {
+	case swipeOnboarding(Int)
+}
+
+struct OnboardingEnvironment {
+	
+}
+
+let onBoardingReducer = Reducer<OnboardingState, OnboardingAction, OnboardingEnvironment> { state, action, environment in
+	switch action {
+	case .swipeOnboarding(let page):
+		return .none
+	}
+}
--- TiquiTaca_iOS/Views/OnboardingView/OnboardingView.swift
+++ TiquiTaca_iOS/Views/OnboardingView/OnboardingView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct OnboardingView: View {
 	@State var currentPage = 0
@@ -13,37 +14,36 @@ struct OnboardingView: View {
 		VStack(alignment: .center, spacing: 8) {
 			VStack {
 				PageControl(numberOfPages: 3, currentPage: $currentPage)
-				
 				Text("온보딩 타이틀")
 					.font(.system(size: 24))
 					.fontWeight(.semibold)
-				
 				Text("서브 타이틀이구여\n서브타이틀이지요")
 					.font(.system(size: 14))
 					.fontWeight(.regular)
 					.padding(.vertical, 8)
-				
-				HStack {
-				}
-				.frame(width: 300, height: 300, alignment: .center)
-				.background(.gray)
-				.cornerRadius(16)
+				LazyHStack { }
+					.frame(maxWidth: .infinity, maxHeight: 300, alignment: .center)
+					.background(.gray)
+					.cornerRadius(16)
 			}.vCenter()
 			
-			
 			VStack(spacing: 24) {
-				Button("이미 계정이 있다면? 로그인"){ }
-					.foregroundColor(.gray)
-				
+				Button(action: { }) {
+					Text("이미 계정이 있다면? ") + Text("로그인").fontWeight(.heavy)
+				}
+				.foregroundColor(.gray)
+					
 				Button("시작하기") { }
 					.frame(maxWidth: .infinity, maxHeight: 56)
-					.cornerRadius(16)
 					.foregroundColor(.white)
 					.background(.gray)
+					.cornerRadius(16)
 			}
 		}
 		.vBottom()
-		.padding(10)
+		.padding(.bottom, 24)
+		.padding(.horizontal, 16)
+		
 	}
 }
 
