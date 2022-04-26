158fa6ee96d8f609b605cc8a58188bbd4929d57c
Minseok Kang <qbq5000@gmail.com>
2022-04-27 01:45:54 +0900

    Add FCM (#30)
    * ✨ Firebase 라이브러리 추가
    
    * ✨ FCM 추가

--- TiquiTaca_iOS/AppDelegate.swift
+++ TiquiTaca_iOS/AppDelegate.swift
@@ -4,15 +4,94 @@
 //
 //  Created by 강민석 on 2022/04/17.
 //
+//
 
 import UIKit
+import Firebase
+import UserNotifications
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+@main
+final class AppDelegate: NSObject, UIApplicationDelegate {
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    print("Called: didFinishLaunchingWithOptions")
+    FirebaseApp.configure()
+    Messaging.messaging().delegate = self
+    UNUserNotificationCenter.current().delegate = self
+    
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, error in
+      if granted {
+        print("알림 등록 완료")
+      } else if let error = error {
+        print("D'oh: \(error.localizedDescription)")
+      }
+    }
+    application.registerForRemoteNotifications()
     return true
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
+  
+  func application(
+    _ application: UIApplication,
+    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    Messaging.messaging().appDidReceiveMessage(userInfo)
+    
+    completionHandler(.noData)
+  }
+  
+  // MARK: UISceneSession Lifecycle
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+  func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+  }
+}
+
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+    
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+  func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    let userInfo = notification.request.content.userInfo
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    Messaging.messaging().appDidReceiveMessage(userInfo)
+    
+    completionHandler([.sound, .banner, .list])
+  }
+  
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    let userInfo = response.notification.request.content.userInfo
+    // With swizzling disabled you must let Messaging know about the message, for Analytics
+    Messaging.messaging().appDidReceiveMessage(userInfo)
+    
+    completionHandler()
   }
 }
