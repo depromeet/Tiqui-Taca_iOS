e2c934c39a5d737ee624cfeb6ecee98d243c18c1
hakyung9712 <hakyungsong9712@gmail.com>
2022-05-28 18:09:01 +0900

    ✨Realm 모델 추가

---
+++ TiquiTaca_iOS/Models/Realm/Message.swift
@@ -0,0 +1,27 @@
+//
+//  Message.swift
+//  TiquiTaca_iOS
+//
+//  Created by 송하경 on 2022/05/28.
+//
+
+import RealmSwift
+
+class Message: Object {
+  enum MessageType: Int {
+    case text = 0
+    case question = 1
+  }
+  
+  @objc dynamic var roomId: String = ""
+  @objc dynamic var senderNickname: String?
+  @objc dynamic var senderProfileImage: String?
+  @objc dynamic var senderInsideFlag: String?
+  
+  @objc dynamic var receivedMessage: String?
+  @objc dynamic var receivedAt: Date?
+  @objc dynamic var sentMessage: String?
+  @objc dynamic var sentAt: Date?
+  
+  var messageType: MessageType?
+}
