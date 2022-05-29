fc807b00ff596ff70b514289dee0316374d05966
hakyung9712 <hakyungsong9712@gmail.com>
2022-05-29 19:12:45 +0900

    ✨ 채팅 > 질문 목록, 질문 상세, 질문 항목, 답글 항목 신규 추가

--- TiquiTaca_iOS/Views/ChatMenu/Question/QuestionItemView.swift
+++ TiquiTaca_iOS/Views/ChatMenu/Question/QuestionItemView.swift
@@ -6,17 +6,52 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+import TTDesignSystemModule
 
 struct QuestionItemView: View {
+  typealias State = QuestionItemState
+  typealias Action = QuestionItemAction
+  
+  private let store: Store<State, Action>
+  @ObservedObject private var viewStore: ViewStore<ViewState, Action>
+  
+  struct ViewState: Equatable {
+    var id: String
+    var user: UserEntity.Response?
+    var content: String
+    var commentList: [CommentEntity]
+    var createdAt: Date
+    var likesCount: Int
+    var commentsCount: Int
+    var ilike: Bool
+    
+    init(state: State) {
+      id = state.id
+      user = state.user
+      content = state.content
+      commentList = state.commentList
+      createdAt = state.createdAt
+      likesCount = state.likesCount
+      commentsCount = state.commentsCount
+      ilike = state.ilike
+    }
+  }
+  
+  init(store: Store<State, Action>) {
+    self.store = store
+    viewStore = ViewStore.init(store.scope(state: ViewState.init))
+  }
+  
   var body: some View {
     VStack(alignment: .leading) {
       HStack(alignment: .top) {
-        Image("defaultProfile")
+        Image(viewStore.user?.profile.imageName ?? "defaultProfile")
           .resizable()
           .frame(width: 32, height: 32)
         
         VStack(alignment: .leading) {
-          Text("안녕하세요")
+          Text(viewStore.user?.nickname ?? "")
             .font(.body4)
             .foregroundColor(.black900)
           Text("1시간 전")
@@ -29,41 +64,45 @@ struct QuestionItemView: View {
       }
       
       VStack {
-        Text("안녕하세여 ㅎㅅㅎ")
+        Text(viewStore.content)
           .font(.body3)
           .foregroundColor(.black900)
         HStack {
           Button {
-            
+            viewStore.send(.likeClickAction)
           } label: {
-            Image("reply_good_on")
-            Text("0")
+            Image(viewStore.ilike ? "reply_good_on" : "reply_good_off")
+            Text("\(viewStore.likesCount)")
               .font(.body7)
               .foregroundColor(.white800)
           }
           
-          Button {
-            
-          } label: {
-            Image("question_reply")
-            Text("0")
-              .font(.body7)
-              .foregroundColor(.white800)
-          }
-          
+          Image("question_comment")
+          Text("\(viewStore.commentsCount)")
+            .font(.body7)
+            .foregroundColor(.white800)
         }
       }
-      
     }
     .padding(.top, 10)
     .padding([.leading, .trailing], 12)
     .padding(.bottom, 16)
     .background(Color.white100)
+    .cornerRadius(14)
   }
 }
 
 struct QuestionItemView_Previews: PreviewProvider {
   static var previews: some View {
-    QuestionItemView()
+    QuestionItemView(
+      store: .init(
+        initialState: QuestionItemState(),
+        reducer: questionItemReducer,
+        environment: QuestionItemEnvironment(
+          appService: AppService(),
+          mainQueue: .main
+        )
+      )
+    )
   }
 }
