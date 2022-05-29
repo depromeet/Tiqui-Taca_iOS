0cef2e07b6390598abe2a4144680141ac3243c15
KangMinseok <qbq5000@gmail.com>
2022-05-29 21:31:04 +0900

    💄 인기채팅방리스트 아이템 디자인 구성

--- TiquiTaca_iOS/Views/MainMap/PopularChatRoomList/PopularChatRoomListItem.swift
+++ TiquiTaca_iOS/Views/MainMap/PopularChatRoomList/PopularChatRoomListItem.swift
@@ -8,29 +8,43 @@
 import SwiftUI
 
 struct PopularChatRoomListItem: View {
-  
   var body: some View {
-    HStack {
+    HStack(spacing: .spacingS) {
       ZStack {
         Image("colorStar")
+          .resizable()
+          .frame(width: 32, height: 32)
+          .padding(.spacingXXS)
         Text("1")
+          .font(.heading3)
       }
-      VStack {
+      VStack(alignment: .leading, spacing: .spacingXXXS) {
         Text("한양대학교")
-        Text("대학교 | 900m")
+          .font(.heading3)
+          .foregroundColor(.white)
+        Text("대학교" + " | " + "900" + "m")
+          .font(.body3)
+          .foregroundColor(.black100)
       }
-      VStack {
+      Spacer()
+      VStack(spacing: 0) {
         Image("peopleColor")
           .resizable()
-          .frame(width: <#T##CGFloat?#>, height: <#T##CGFloat?#>)
-        Text("300명")
+          .frame(width: 24, height: 24)
+        Text("300" + "명")
+          .font(.subtitle4)
+          .foregroundColor(.white500)
       }
     }
+    .padding(.vertical, .spacingS)
+    .padding(.horizontal, .spacingXL)
   }
 }
 
 struct PopularChatRoomListItem_Previews: PreviewProvider {
   static var previews: some View {
     PopularChatRoomListItem()
+      .background(Color.black700)
+      .previewLayout(.sizeThatFits)
   }
 }
