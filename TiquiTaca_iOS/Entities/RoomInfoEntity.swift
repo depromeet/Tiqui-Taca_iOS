707aabc82d0b668242a298ad05cedf1cd894bff4
KangMinseok <qbq5000@gmail.com>
2022-05-31 00:13:43 +0900

    ✨ RoomInfoEntity 좌표값 추가

--- TiquiTaca_iOS/Entities/RoomInfoEntity.swift
+++ TiquiTaca_iOS/Entities/RoomInfoEntity.swift
@@ -14,28 +14,36 @@ enum RoomInfoEntity {
     let id: String?
     let name: String?
     let category: LocationCategory?
-    let userCount: Int?
+    let userCount: Int
+    let latitude: Double
+    let longitude: Double
     
     enum CodingKeys: String, CodingKey {
       case id = "_id"
       case name
       case category
       case userCount
+      case latitude = "lat"
+      case longitude = "lng"
     }
     
     init() {
       id = nil
       name = nil
       category = nil
-      userCount = nil
+      userCount = 0
+      latitude = 0
+      longitude = 0
     }
     
     init(from decoder: Decoder) throws {
       let container = try decoder.container(keyedBy: CodingKeys.self)
-      id = (try? container.decode(String.self, forKey: .id)) ?? ""
-      name = (try? container.decode(String.self, forKey: .name)) ?? ""
+      id = try? container.decode(String.self, forKey: .id)
+      name = try? container.decode(String.self, forKey: .name)
       category = try? container.decode(LocationCategory.self, forKey: .category)
-      userCount = (try? container.decode(Int.self, forKey: .userCount)) ?? 1
+      userCount = (try? container.decode(Int.self, forKey: .userCount)) ?? 0
+      latitude = (try? container.decode(Double.self, forKey: .latitude)) ?? 0
+      longitude = (try? container.decode(Double.self, forKey: .longitude)) ?? 0
     }
   }
 }
