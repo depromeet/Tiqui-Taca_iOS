d7d3608e5c32f90166870ee163beb83bd2dd5b7b
KangMinseok <qbq5000@gmail.com>
2022-06-01 01:25:06 +0900

    ✨ BottomSheet 사용을 위한 설정값 정의

---
+++ TiquiTaca_iOS/Entities/TTBottomSheet.swift
@@ -0,0 +1,28 @@
+//
+//  TTBottomSheet.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/05/31.
+//
+
+import SwiftUI
+import BottomSheetSwiftUI
+
+enum TTBottomSheet {
+  enum Position: CGFloat, CaseIterable {
+    case top = 0.9
+    case middle = 0.5
+    case hidden = 0
+  }
+  
+  static let Options: [BottomSheet.Options] = [
+    .allowContentDrag,
+    .swipeToDismiss,
+    .tapToDismiss,
+    .dragIndicatorColor(.white),
+    .noBottomPosition,
+    .cornerRadius(48),
+    .shadow(color: .black.opacity(0.25), radius: 40, x: 0, y: -4),
+    .background({ AnyView(Color.black700) })
+  ]
+}
