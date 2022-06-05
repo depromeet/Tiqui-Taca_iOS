ee5d7fcb89ed45ad84f9a8ab724674304d0c3f49
KangMinseok <qbq5000@gmail.com>
2022-06-05 20:35:43 +0900

    ✨ 채팅방 리스트 기능추가

---
+++ TiquiTaca_iOS/Views/MainMap/ChatRoomList/ChatRoomListCore.swift
@@ -0,0 +1,104 @@
+//
+//  ChatRoomListCore.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/06/05.
+//
+
+import ComposableArchitecture
+import ComposableCoreLocation
+import TTNetworkModule
+
+struct ChatRoomListState: Equatable {
+  var listSortType: ChatRoomListSortType = .distance
+  var listCategoryType: LocationCategory = .favorite
+  var chatRoomList: [RoomFromCategoryResponse] = []
+  var isLoading: Bool = false
+  var currentLocation: CLLocation = .init(latitude: 0, longitude: 0)
+}
+
+enum ChatRoomListAction: Equatable {
+  case setListSortType(ChatRoomListSortType)
+  case itemSelected(RoomFromCategoryResponse)
+  case getRoomListResponse(Result<[RoomFromCategoryResponse]?, HTTPError>)
+  case requestChatRoomList
+  case setLoading(Bool)
+  case deleteItem(IndexSet)
+}
+
+struct ChatRoomListEnvironment {
+  let appService: AppService
+  let mainQueue: AnySchedulerOf<DispatchQueue>
+}
+
+let chatRoomListReducer = Reducer<
+  ChatRoomListState,
+  ChatRoomListAction,
+  ChatRoomListEnvironment
+>.combine([
+  chatRoomListCore
+])
+
+let chatRoomListCore = Reducer<
+  ChatRoomListState,
+  ChatRoomListAction,
+  ChatRoomListEnvironment
+> { state, action, environment in
+  switch action {
+  case let .setListSortType(type):
+    if state.listSortType == type { return .none }
+    state.listSortType = type
+    return sortChatRoomList(&state)
+    
+  case .itemSelected:
+    return .none
+    
+  case let .getRoomListResponse(.success(response)):
+    guard let roomList = response else { return .none }
+    state.chatRoomList = roomList
+    return sortChatRoomList(&state)
+    
+  case .getRoomListResponse(.failure):
+    return .none
+    
+  case .requestChatRoomList:
+    let request = RoomFromCategoryRequest(
+      latitude: state.currentLocation.coordinate.latitude,
+      longitude: state.currentLocation.coordinate.longitude,
+      filter: state.listCategoryType,
+      radius: 10000
+    )
+    return .concatenate([
+      .init(value: .setLoading(true)),
+      environment.appService.roomService
+        .getRoomList(request)
+        .receive(on: environment.mainQueue)
+        .catchToEffect()
+        .map(ChatRoomListAction.getRoomListResponse)
+      ,
+      .init(value: .setLoading(false))
+    ])
+    
+  case let .setLoading(isLoading):
+    state.isLoading = isLoading
+    return .none
+    
+  case let .deleteItem(offsets):
+    state.chatRoomList.remove(atOffsets: offsets)
+    return .none
+  }
+}
+
+private func sortChatRoomList(_ state: inout ChatRoomListState) -> Effect<ChatRoomListAction, Never> {
+  switch state.listSortType {
+  case .distance:
+    state.chatRoomList = state.chatRoomList.sorted {
+      $0.distance(from: state.currentLocation) < $1.distance(from: state.currentLocation)
+    }
+  case .popular:
+    state.chatRoomList = state.chatRoomList.sorted {
+      $0.userCount > $1.userCount
+    }
+  }
+  return .none
+}
---
+++ TiquiTaca_iOS/Views/MainMap/ChatRoomList/ChatRoomListView.swift
@@ -0,0 +1,146 @@
+//
+//  ChatRoomListView.swift
+//  TiquiTaca_iOS
+//
+//  Created by 강민석 on 2022/06/05.
+//
+
+import SwiftUI
+import CoreLocation
+import ComposableArchitecture
+import TTDesignSystemModule
+
+struct ChatRoomListView: View {
+  typealias State = ChatRoomListState
+  typealias Action = ChatRoomListAction
+  
+  private let store: Store<State, Action>
+  private var viewStore: ViewStore<ViewState, Action>
+  
+  var distanceColor: Color {
+    if viewStore.listSortType == .distance {
+      return .white
+    } else {
+      return .black100
+    }
+  }
+  var popularColor: Color {
+    if viewStore.listSortType == .popular {
+      return .white
+    } else {
+      return .black100
+    }
+  }
+  
+  struct ViewState: Equatable {
+    let listSortType: ChatRoomListSortType
+    let listCategoryType: LocationCategory
+    let chatRoomList: [RoomFromCategoryResponse]
+    let isLoading: Bool
+    let currentLocation: CLLocation
+    
+    init(state: State) {
+      listSortType = state.listSortType
+      listCategoryType = state.listCategoryType
+      chatRoomList = state.chatRoomList
+      isLoading = state.isLoading
+      currentLocation = state.currentLocation
+    }
+  }
+  
+  init(store: Store<State, Action>) {
+    self.store = store
+    viewStore = ViewStore(store.scope(state: ViewState.init))
+  }
+  
+  var body: some View {
+    ZStack {
+      VStack(spacing: 0) {
+        HStack {
+          HStack(spacing: .spacingXXS) {
+            Image("starLight")
+              .resizable()
+              .frame(width: 32, height: 32)
+            Text(viewStore.listCategoryType.locationName)
+              .font(.subtitle1)
+              .foregroundColor(.green500)
+            Text("\(viewStore.chatRoomList.count)")
+              .font(.subtitle2)
+              .foregroundColor(.black100)
+          }
+          Spacer()
+          HStack(spacing: .spacingS) {
+            Button {
+              viewStore.send(.setListSortType(.distance))
+            } label: {
+              Text("거리순")
+                .foregroundColor(distanceColor)
+            }
+            Button {
+              viewStore.send(.setListSortType(.popular))
+            } label: {
+              Text("인기순")
+                .foregroundColor(popularColor)
+            }
+          }
+          .font(.body2)
+        }
+        .padding(.horizontal, .spacingXL)
+        ZStack {
+          List {
+            ForEach(viewStore.chatRoomList) { element in
+              Button {
+                viewStore.send(.itemSelected(element))
+              } label: {
+                ChatRoomListItem(
+                  roomInfo: element,
+                  currentLocation: viewStore.currentLocation
+                )
+              }
+              .deleteDisabled(viewStore.listCategoryType != .favorite)
+            }
+            .onDelete { offsets in
+              viewStore.send(.deleteItem(offsets))
+            }
+          }
+          if viewStore.chatRoomList.isEmpty {
+            VStack(spacing: .spacingS) {
+              Image("bxInfoArrow")
+                .resizable()
+                .frame(width: 108, height: 108)
+              Text("채팅방이 없어요.")
+                .font(.body2)
+                .foregroundColor(.black100)
+                .multilineTextAlignment(.center)
+            }
+            .padding(.bottom, 80)
+          }
+        }
+      }
+      TTIndicator(
+        style: .medium,
+        isAnimating: viewStore.binding(
+          get: \.isLoading,
+          send: Action.setLoading
+        )
+      )
+    }
+    .vCenter()
+    .hCenter()
+  }
+}
+
+struct ChatRoomListView_Previews: PreviewProvider {
+  static var previews: some View {
+    ChatRoomListView(
+      store: .init(
+        initialState: .init(),
+        reducer: chatRoomListReducer,
+        environment: .init(
+          appService: .init(),
+          mainQueue: .main
+        )
+      )
+    )
+  }
+}
