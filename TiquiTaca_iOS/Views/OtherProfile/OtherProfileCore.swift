7b50de32122e47c7ebacfd88f96907f1e50d23a7
RokwonK <wonrok97@naver.com>
2022-06-08 22:37:21 +0900

    💄 receivedMessageView 닉네임 길때 시간이 밀리는 UI버그 개선

---
+++ TiquiTaca_iOS/Views/OtherProfile/OtherProfileCore.swift
@@ -0,0 +1,8 @@
+//
+//  OtherProfileCore.swift
+//  TiquiTaca_iOS
+//
+//  Created by 김록원 on 2022/06/07.
+//
+
+import Foundation
---
+++ TiquiTaca_iOS/Views/OtherProfile/OtherProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  OtherProfileView.swift
+//  TiquiTaca_iOS
+//
+//  Created by 김록원 on 2022/06/07.
+//
+
+import SwiftUI
+
+struct OtherProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OtherProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        OtherProfileView()
+    }
+}
