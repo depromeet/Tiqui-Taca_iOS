2a32a2932c6730e4a9f07d7a9854272897e468e1
MinseokKang <qbq5000@gmail.com>
2022-07-02 02:38:35 +0900

    전화번호 입력 textfield 디자인 수정

--- TiquiTaca_iOS/Extensions/View+Extensions.swift
+++ TiquiTaca_iOS/Extensions/View+Extensions.swift
@@ -133,3 +133,16 @@ struct BackgroundClearView: UIViewRepresentable {
   func updateUIView(_ uiView: UIView, context: Context) {
   }
 }
+
+extension View {
+  func placeholder<Content: View>(
+    when shouldShow: Bool,
+    alignment: Alignment = .leading,
+    @ViewBuilder placeholder: () -> Content) -> some View {
+      
+      ZStack(alignment: alignment) {
+        placeholder().opacity(shouldShow ? 1 : 0)
+        self
+      }
+    }
+}
