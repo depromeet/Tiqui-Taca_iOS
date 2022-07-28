5381f85c19322789dbcb7f9b65408bbd561f196b
MinseokKang <qbq5000@gmail.com>
2022-07-28 23:34:27 +0900

    관련 모델 수정

--- TiquiTaca_iOS/Entities/Response/CategoryEntity.swift
+++ TiquiTaca_iOS/Entities/Response/CategoryEntity.swift
@@ -1,5 +1,5 @@
 //
-//  CategoryListResponse.swift
+//  CategoryEntity.swift
 //  TiquiTaca_iOS
 //
 //  Created by MinseokKang on 2022/07/24.
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CategoryListResponse: Codable, Equatable {
+struct CategoryEntity: Codable, Equatable, Identifiable {
   var id: String
   var name: String
   var imageUrl: URL?
@@ -17,5 +17,17 @@ struct CategoryListResponse: Codable, Equatable {
     id = (try? container.decode(String.self, forKey: .id)) ?? ""
     name = (try? container.decode(String.self, forKey: .name)) ?? ""
     imageUrl = try? container.decode(URL.self, forKey: .imageUrl)
+  }
+}
+
+extension CategoryEntity {
+  /// 전체 타입 여부
+  var isAllType: Bool {
+    return id == "ALL"
+  }
+  
+  /// 즐겨찾기 타입 여부
+  var isFavoriteType: Bool {
+    return id == "FAVORITE"
   }
 }
